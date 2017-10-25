d0e0a02a2000f7a110b7017dfa78d7a77394e41e
geoniced <barbos1701@gmail.com>
2017-10-25 07:57:51 -0700

    Concept version ready (no styles)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  def completed?
+    # Item is completed if `completed_at` is NOT blank!
+    # Meaning if it's not null (nil)
+    !completed_at.blank?
+  end
 end
